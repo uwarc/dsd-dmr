a687cb9aa1577cae5f45140eec1abefc53f61be9
Peter Barfuss <pbarfuss@corn-syrupcorn-syrup>
2015-01-16 13:21:01 -0500

    Fix X2-TDMA data frame sync so it actually works again.

--- dsd_frame_sync.c
+++ dsd_frame_sync.c
@@ -355,6 +355,16 @@ getFrameSync (dsd_opts * opts, dsd_state * state)
                   strcpy (state->ftype, " (P25p1)   ");
                   state->lastsynctype = -1;
                   return (1);
+              } else if ((state->lastsynctype == 4) && 
+                         ((memcmp (synctest, X2TDMA_BS_DATA_SYNC, 24) != 0) ||
+                          (memcmp (synctest, X2TDMA_MS_DATA_SYNC, 24) != 0))) {  
+                  state->carrier = 1;
+                  state->offset = synctest_pos;
+                  state->max = ((state->max) + lmax) / 2;
+                  state->min = ((state->min) + lmin) / 2;
+                  strcpy (state->ftype, "(X2-TDMA)  ");
+                  state->lastsynctype = -1;
+                  return (4);
               } else if ((state->lastsynctype == 11) &&
                   ((memcmp (synctest, DMR_BS_VOICE_SYNC, 24) != 0) || (memcmp (synctest, DMR_MS_VOICE_SYNC, 24) != 0))) {
                   state->carrier = 1;
