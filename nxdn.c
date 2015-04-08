e687ad444cc430be277267fde0fa047e2c98a641
Peter Barfuss <pbarfuss@corn-syrupcorn-syrup>
2015-04-07 21:31:24 -0400

    Print out SACCH (need some samples to test convolutional coder, ugh I hate convolutional codes), move data decoding into dsd_frame.c - mostly as I'm not intending to support NXDN data decoding anytime soon. DMR/MotoTRBO+variants and p25p1/p2 is enough work already. :P

--- nxdn.c
+++ nxdn.c
@@ -1,63 +1,45 @@
 #include "dsd.h"
 #include "nxdn_const.h"
 
-void
-processNXDNData (dsd_opts * opts, dsd_state * state)
-{
-  unsigned int i, dibit;
-  unsigned char *dibit_p;
-  char lich[9];
-  char lich_scram[9] = { 0, 0, 1, 0, 0, 1, 1, 1, 0 };
-  dibit_p = state->dibit_buf_p - 8;
-
-  for (i = 0; i < 8; i++) {
-      dibit = *dibit_p++;
-      if(lich_scram[i] ^ (state->lastsynctype & 0x1)) {
-          dibit = (dibit ^ 2);
-      }
-      lich[i] = 1 & (dibit >> 1);
-  }
-
-  closeMbeOutFile (opts, state);
-  state->errs2 = 0;
-
-  if (opts->errorbars) {
-      int level = (int) state->max / 164;
-      printf ("Sync: %s mod: GFSK      inlvl: %2i%% %s %s  DATA: %s Ch\n",
-              state->ftype, level, state->slot0light, state->slot1light,
-              (lich[1] ? (lich[0] ? "Trunk-C Traffic" : "Trunk-D Composite") 
-                       : (lich[0] ? "Trunk-C Control" : (lich[6] ? "Repeater" : "Mobile Direct"))));
-  }
-
-  skipDibit (opts, state, 174);
-}
-
 /*
  *  pseudorandom bit sequence
  */
-const char nxdnpr[145] = {
-    1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0,
-    1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1,
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1,
-    1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0,
-    0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-    0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0,
-    1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0,
-    0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1,
-    0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1
+const char nxdnpr[175] = {
+    0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1,
+    0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0,
+    1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1,
+    1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
+    0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+    0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0,
+    1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1
 };
 
 unsigned int
 processNXDNVoice (dsd_opts * opts, dsd_state * state)
 {
-  int i, j;
-  unsigned int dibit, total_errs = 0;
+  unsigned int i, j, l1, l2, dibit, total_errs = 0;
+  unsigned char sacch[60];
+  //unsigned char sacch_out[20];
   char ambe_fr[4][24];
   const char *pr;
-
-  skipDibit (opts, state, 30);
-
   pr = nxdnpr;
+
+  for (i = 0; i < 30; i++) {
+    dibit = getDibit (opts, state);
+    sacch[nsW[i]] = *pr ^ (1 & (dibit >> 1)); // bit 1
+    sacch[nsY[i]] = (1 & dibit);        // bit 0
+    pr++;
+  }
+  //nxdn_conv_sacch_decode(sacch, sacch_out);
+  //l = get_uint(sacch_out, 20);
+  l1 = get_uint(sacch, 30);
+  l2 = get_uint(sacch, 30);
+  printf("NXDN: SACCH: 0x%08x%08x\n", l1, l2); 
+ 
   for (j = 0; j < 4; j++) {
     for (i = 0; i < 36; i++) { 
       dibit = getDibit (opts, state);
