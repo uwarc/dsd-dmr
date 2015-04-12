07884f448429ee521af959396954bea8762b6322
Peter Barfuss <pbarfuss@corn-syrupcorn-syrup>
2015-04-12 09:23:30 -0400

    Just print the full NID only.

--- bchenc.c
+++ bchenc.c
@@ -10,10 +10,8 @@ int main(int argc, char **argv) {
     uint16_t nac_duid_test, nac_duid_in = (hdr & 0xFFFF);
     uint64_t encoded_nac_duid = bchEnc(nac_duid_in);
     int nerrs = bchDec(encoded_nac_duid, &nac_duid_test);
-    fprintf(stderr, "In: 0x%04x (NAC: 0x%X, DUID: 0x%X) -> 0x%08lx -> 0x%04x\n",
-            nac_duid_in, (nac_duid_in >> 4), (nac_duid_in & 0x0F),
-            encoded_nac_duid, nac_duid_test);
-    fprintf(stderr, "nerrs: %d\n", nerrs); 
+    fprintf(stderr, "In: 0x%04x -> 0x%08lx -> 0x%04x (nerrs: %d)\n",
+            nac_duid_in, encoded_nac_duid, nac_duid_test, nerrs);
     return 0;
 }
 
